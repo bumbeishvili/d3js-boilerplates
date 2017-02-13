2b43fc979fae5f2ba7d2e066e4b13773472907f7
Dato Bumbeishvili <bumbeishvili@users.noreply.github.com>
2017-02-14 01:40:24 +0400

    Create sample.js

---
+++ Reusable/sample.js
@@ -0,0 +1,97 @@
+
+
+
+function getChart() {
+    // exposed variables
+    var attrs = {
+        width: null,
+        height: null,
+        chartWidth: null,
+        chartHeight: null,
+        marginTop,
+        marginBottom,
+        marginRight,
+        marginLeft,
+        data: null
+    };
+
+
+    /*############### IF EXISTS OVERWRITE ATTRIBUTES FROM PASSED PARAM  #######  */
+
+    var attrKeys = Object.keys(attrs);
+    attrKeys.forEach(function (key) {
+        if (params[key]) {
+            attrs[key] = params[key];
+        }
+    })
+
+
+    //innerFunctions
+    var updateData;
+
+
+    //main chart object
+    var chart = function (selection) {
+        selection.each(function () {
+
+            //calculated properties
+            var calc = {}
+
+            calc.chartLeftMargin = attrs.marginLeft;
+            calc.chartTopMargin = attrs.marginTop;
+
+            calc.chartWidth = attrs.svgWidth - attrs.marginRight - calc.chartLeftMargin;
+            calc.chartHeight = attrs.svgHeight - attrs.marginBottom - calc.chartTopMargin;
+
+            //drawing
+            var svg = d3.select(this)
+                .append('svg')
+                .attr("viewBox", "0 0 " + attrs.svgWidth + " " + attrs.svgHeight)
+                .attr("preserveAspectRatio", "xMidYMid meet")
+
+
+            var chart = svg.append('g')
+                .attr('width', calc.chartWidth)
+                .attr('height', calc.chartHeight)
+                .attr('transform', 'translate(' + (calc.chartLeftMargin) + ',' + calc.chartTopMargin + ')')
+
+
+
+
+
+            // smoothly handle data updating
+            updateData = function () {
+
+
+            }
+
+
+        });
+    }
+
+
+    //exposed variable funcs
+    chart.data = function (value) {
+        if (!arguments.length) return attrs.data;
+        attrs.data = value;
+        if (typeof updateData === 'function') {
+            updateData();
+        }
+        return chart;
+    }
+
+    chart.width = function (value) {
+        if (!arguments.length) return attrs.width;
+        attrs.width = value;
+        return chart;
+    }
+
+    chart.height = function (value) {
+        if (!arguments.length) return attrs.height;
+        attrs.height = value;
+        return chart;
+    }
+
+
+    return chart;
+}
