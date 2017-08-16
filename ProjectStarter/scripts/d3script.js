d3a39bf6f74ccc5d0166cb150a1e3f16c2339e7c
Davit Bumbeishvili <dbumbeishvili@bog.ge>
2017-08-16 10:30:08 +0400

    apply insert update pattern

--- ProjectStarter/scripts/d3script.js
+++ ProjectStarter/scripts/d3script.js
@@ -2,100 +2,99 @@
 
 
 function renderChart(params) {
-    // exposed variables
-    var attrs = {
-        svgWidth: 400,
-        svgHeight: 400,
-        marginTop: 5,
-        marginBottom: 5,
-        marginRight: 5,
-        marginLeft: 5,
-        data: null
-    };
+  // exposed variables
+  var attrs = {
+    svgWidth: 400,
+    svgHeight: 400,
+    marginTop: 5,
+    marginBottom: 5,
+    marginRight: 5,
+    marginLeft: 5,
+    data: null
+  };
 
+  /*############### IF EXISTS OVERWRITE ATTRIBUTES FROM PASSED PARAM  #######  */
 
-    /*############### IF EXISTS OVERWRITE ATTRIBUTES FROM PASSED PARAM  #######  */
-
-    var attrKeys = Object.keys(attrs);
-    attrKeys.forEach(function (key) {
-        if (params && params[key]) {
-            attrs[key] = params[key];
-        }
-    })
-
-
-    //innerFunctions
-    var updateData;
-
-
-    //main chart object
-    var main = function (selection) {
-        selection.each(function () {
-
-            //calculated properties
-            var calc = {}
-
-            calc.chartLeftMargin = attrs.marginLeft;
-            calc.chartTopMargin = attrs.marginTop;
-
-            calc.chartWidth = attrs.svgWidth - attrs.marginRight - calc.chartLeftMargin;
-            calc.chartHeight = attrs.svgHeight - attrs.marginBottom - calc.chartTopMargin;
-
-            //drawing
-            var svg = d3.select(this)
-                .append('svg')
-                .attr('width', attrs.svgWidth)
-                .attr('height', attrs.svgHeight)
-            // .attr("viewBox", "0 0 " + attrs.svgWidth + " " + attrs.svgHeight)
-            // .attr("preserveAspectRatio", "xMidYMid meet")
-
-
-            var chart = svg.append('g')
-                .attr('width', calc.chartWidth)
-                .attr('height', calc.chartHeight)
-                .attr('transform', 'translate(' + (calc.chartLeftMargin) + ',' + calc.chartTopMargin + ')')
-
-
-
-
-
-            // smoothly handle data updating
-            updateData = function () {
-
-
-            }
-
-
-        });
-    };
-
-
-
-
-
-    ['svgWidth', 'svgHeight'].forEach(key => {
-        // Attach variables to main function
-        return main[key] = function (_) {
-            var string = `attrs['${key}'] = _`;
-            if (!arguments.length) { eval(`return attrs['${key}']`); }
-            eval(string);
-            return main;
-        };
-    });
-
-
-
-
-    //exposed update functions
-    main.data = function (value) {
-        if (!arguments.length) return attrs.data;
-        attrs.data = value;
-        if (typeof updateData === 'function') {
-            updateData();
-        }
-        return main;
+  var attrKeys = Object.keys(attrs);
+  attrKeys.forEach(function (key) {
+    if (params && params[key]) {
+      attrs[key] = params[key];
     }
+  })
+
+  //innerFunctions
+  var updateData;
+
+  //main chart object
+  var main = function (selection) {
+    selection.each(function () {
+
+      //calculated properties
+      var calc = {}
+      calc.chartLeftMargin = attrs.marginLeft;
+      calc.chartTopMargin = attrs.marginTop;
+      calc.chartWidth = attrs.svgWidth - attrs.marginRight - calc.chartLeftMargin;
+      calc.chartHeight = attrs.svgHeight - attrs.marginBottom - calc.chartTopMargin;
+
+      //drawing
+      var container = d3.select(this);
+
+      var svg = patternify({ container: container, selector: 'svg-chart-container', elementTag: 'svg' })
+      svg.attr('width', attrs.svgWidth)
+        .attr('height', attrs.svgHeight)
+      // .attr("viewBox", "0 0 " + attrs.svgWidth + " " + attrs.svgHeight)
+      // .attr("preserveAspectRatio", "xMidYMid meet")
 
 
+      var chart = patternify({ container: svg, selector: 'chart', elementTag: 'g' })
+      chart.attr('transform', 'translate(' + (calc.chartLeftMargin) + ',' + calc.chartTopMargin + ')');
+
+
+      // smoothly handle data updating
+      updateData = function () {
+
+
+      }
+
+      //#########################################  UTIL FUNCS ##################################
+
+      //enter exit update pattern principle
+      function patternify(params) {
+        var container = params.container;
+        var selector = params.selector;
+        var elementTag = params.elementTag;
+
+        // pattern in action
+        var selection = container.selectAll('.' + selector).data([selector])
+        selection.exit().remove();
+        selection = selection.enter().append(elementTag).merge(selection)
+        selection.attr('class', selector);
+        return selection;
+      }
+    });
+  };
+
+
+  ['svgWidth', 'svgHeight'].forEach(key => {
+    // Attach variables to main function
+    return main[key] = function (_) {
+      var string = `attrs['${key}'] = _`;
+      if (!arguments.length) { eval(`return attrs['${key}']`); }
+      eval(string);
+      return main;
+    };
+  });
+
+  //exposed update functions
+  main.data = function (value) {
+    if (!arguments.length) return attrs.data;
+    attrs.data = value;
+    if (typeof updateData === 'function') {
+      updateData();
+    }
     return main;
+  }
+
+
+  return main;
 }
