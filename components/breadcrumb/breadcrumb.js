add53ea35b42007b9887ee33aff7998aa954cb00
Dato <davit.bumbeishvili@gmail.com>
2017-11-02 20:50:33 +0400

    prepare breadcrumb and tooltip component

---
+++ components/breadcrumb/breadcrumb.js
@@ -0,0 +1,146 @@
+/*  
+
+This code is based on following convention:
+
+https://github.com/bumbeishvili/d3-coding-conventions
+
+*/
+
+function renderChart(params) {
+
+  // exposed variables
+  var attrs = {
+    svgWidth: 400,
+    svgHeight: 400,
+    marginTop: 5,
+    marginBottom: 5,
+    marginRight: 5,
+    marginLeft: 5,
+    container: 'body',
+    data: null
+  };
+
+  /*############### IF EXISTS OVERWRITE ATTRIBUTES FROM PASSED PARAM  #######  */
+
+  var attrKeys = Object.keys(attrs);
+  attrKeys.forEach(function (key) {
+    if (params && params[key]) {
+      attrs[key] = params[key];
+    }
+  })
+
+  //innerFunctions which will update visuals
+  var updateData;
+
+  //main chart object
+  var main = function (selection) {
+    selection.each(function scope() {
+
+      //calculated properties
+      var calc = {}
+      calc.chartLeftMargin = attrs.marginLeft;
+      calc.chartTopMargin = attrs.marginTop;
+      calc.chartWidth = attrs.svgWidth - attrs.marginRight - calc.chartLeftMargin;
+      calc.chartHeight = attrs.svgHeight - attrs.marginBottom - calc.chartTopMargin;
+
+      //drawing containers
+      var container = d3.select(this);
+
+      //add svg
+      var svg = container.patternify({ tag: 'svg', selector: 'svg-chart-container' })
+        .attr('width', attrs.svgWidth)
+        .attr('height', attrs.svgHeight)
+      // .attr("viewBox", "0 0 " + attrs.svgWidth + " " + attrs.svgHeight)
+      // .attr("preserveAspectRatio", "xMidYMid meet")
+
+      //add container g element
+      var chart = svg.patternify({ tag: 'g', selector: 'chart' })
+        .attr('transform', 'translate(' + (calc.chartLeftMargin) + ',' + calc.chartTopMargin + ')');
+
+      // smoothly handle data updating
+      updateData = function () {
+
+      }
+      //#########################################  UTIL FUNCS ##################################
+
+      function debug() {
+        if (attrs.isDebug) {
+          //stringify func
+          var stringified = scope + "";
+
+          // parse variable names
+          var groupVariables = stringified
+            //match var x-xx= {};
+            .match(/var\s+([\w])+\s*=\s*{\s*}/gi)
+            //match xxx
+            .map(d => d.match(/\s+\w*/gi).filter(s => s.trim()))
+            //get xxx
+            .map(v => v[0].trim())
+
+          //assign local variables to the scope
+          groupVariables.forEach(v => {
+            main['P_' + v] = eval(v)
+          })
+        }
+      }
+      debug();
+    });
+  };
+
+  //----------- PROTOTYEPE FUNCTIONS  ----------------------
+  d3.selection.prototype.patternify = function (params) {
+    var container = this;
+    var selector = params.selector;
+    var elementTag = params.tag;
+    var data = params.data || [selector];
+
+    // pattern in action
+    var selection = container.selectAll('.' + selector).data(data)
+    selection.exit().remove();
+    selection = selection.enter().append(elementTag).merge(selection)
+    selection.attr('class', selector);
+    return selection;
+  }
+
+  //dinamic keys functions
+  Object.keys(attrs).forEach(key => {
+    // Attach variables to main function
+    return main[key] = function (_) {
+      var string = `attrs['${key}'] = _`;
+      if (!arguments.length) { return eval(` attrs['${key}'];`); }
+      eval(string);
+      return main;
+    };
+  });
+
+  //set attrs as property
+  main.attrs = attrs;
+
+  //debugging visuals
+  main.debug = function (isDebug) {
+    attrs.isDebug = isDebug;
+    if (isDebug) {
+      if (!window.charts) window.charts = [];
+      window.charts.push(main);
+    }
+    return main;
+  }
+
+  //exposed update functions
+  main.data = function (value) {
+    if (!arguments.length) return attrs.data;
+    attrs.data = value;
+    if (typeof updateData === 'function') {
+      updateData();
+    }
+    return main;
+  }
+
+  // run  visual
+  main.run = function () {
+    d3.selectAll(attrs.container).call(main);
+    return main;
+  }
+
+  return main;
+}
