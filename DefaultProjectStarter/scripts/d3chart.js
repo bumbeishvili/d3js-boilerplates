ff902b401f6058fe0c4a20b8183c334d26938b05
bumbeishvili <me@davidb.dev>
2022-01-30 16:37:16 +0400

    Add Controls

--- DefaultProjectStarter/scripts/d3chart.js
+++ DefaultProjectStarter/scripts/d3chart.js
@@ -1,165 +1,217 @@
 class Chart {
     constructor() {
-        this.createId = () => Date.now().toString(36) + Math.random().toString(36).substr(2);
-
-        // Declare initial chart state attributes
-        const state = {
-            id: this.createId(),
+        // Defining state attributes
+        const attrs = {
+            id: "ID" + Math.floor(Math.random() * 1000000),
             svgWidth: 400,
-            svgHeight: 400,
+            svgHeight: 200,
             marginTop: 5,
             marginBottom: 5,
             marginRight: 5,
             marginLeft: 5,
-            container: 'body',
-            defaultTextFill: '#2C3E50',
-            defaultFont: 'Helvetica',
-            ctx: document.createElement('canvas').getContext('2d'),
-            data: null
+            container: "body",
+            defaultTextFill: "#2C3E50",
+            defaultFont: "Helvetica",
+            data: null,
+            chartWidth: null,
+            chartHeight: null,
+            firstRender: true,
+            guiEnabled: false,
         };
 
-        // Inner state props
-        Object.assign(state, {
-            calc: null,
-            svg: null,
-            chart: null,
-            d3container: null,
-        })
+        // Defining accessors
+        this.getState = () => attrs;
+        this.setState = (d) => Object.assign(attrs, d);
 
-        // Define state getters and setters
-        this.getState = () => state;
-        this.setState = (d) => { Object.assign(state, d) };
-
-        // Automatically generate getters and setters for each state properties
-        Object.keys(state).forEach((key) => {
+        // Automatically generate getter and setters for chart object based on the state properties;
+        Object.keys(attrs).forEach((key) => {
             //@ts-ignore
             this[key] = function (_) {
-                var string = `state['${key}'] = _`;
                 if (!arguments.length) {
-                    return eval(`state['${key}'];`);
+                    return attrs[key];
                 }
-                eval(string);
+                attrs[key] = _;
                 return this;
             };
         });
 
-        // Define handful d3 enter, exit, update pattern method
+        // Custom enter exit update pattern initialization (prototype method)
         this.initializeEnterExitUpdatePattern();
     }
+    render() {
+        this.addChartGui()
+        this.setDynamicContainer();
+        this.calculateProperties();
+        this.drawSvgAndWrappers();
+        this.drawRects();
+        return this;
+    }
 
-    // Define enter exit update pattern shorthand
+    calculateProperties() {
+        const {
+            marginLeft,
+            marginTop,
+            marginRight,
+            marginBottom,
+            svgWidth,
+            svgHeight
+        } = this.getState();
+
+        //Calculated properties
+        var calc = {
+            id: null,
+            chartTopMargin: null,
+            chartLeftMargin: null,
+            chartWidth: null,
+            chartHeight: null
+        };
+        calc.id = "ID" + Math.floor(Math.random() * 1000000); // id for event handlings
+        calc.chartLeftMargin = marginLeft;
+        calc.chartTopMargin = marginTop;
+        const chartWidth = svgWidth - marginRight - calc.chartLeftMargin;
+        const chartHeight = svgHeight - marginBottom - calc.chartTopMargin;
+
+        this.setState({ calc, chartWidth, chartHeight });
+    }
+
+    drawRects() {
+        const { chart, data, chartWidth, chartHeight } = this.getState();
+
+        chart
+            ._add({
+                tag: "rect",
+                selector: "rect-sample",
+                data: [data]
+            })
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("fill", (d) => d.color);
+    }
+
+    drawSvgAndWrappers() {
+        const {
+            d3Container,
+            svgWidth,
+            svgHeight,
+            defaultFont,
+            calc,
+            data,
+            chartWidth,
+            chartHeight
+        } = this.getState();
+
+        // Draw SVG
+        const svg = d3Container
+            ._add({
+                tag: "svg",
+                className: "svg-chart-container"
+            })
+            .attr("width", svgWidth)
+            .attr("height", svgHeight)
+            .attr("font-family", defaultFont);
+
+        //Add container g element
+        var chart = svg
+            ._add({
+                tag: "g",
+                className: "chart"
+            })
+            .attr(
+                "transform",
+                "translate(" + calc.chartLeftMargin + "," + calc.chartTopMargin + ")"
+            );
+
+        chart
+            ._add({
+                tag: "rect",
+                selector: "rect-sample",
+                data: [data]
+            })
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("fill", (d) => d.color);
+
+        this.setState({ chart, svg });
+    }
+
     initializeEnterExitUpdatePattern() {
-        d3.selection.prototype.patternify = function (params) {
+        d3.selection.prototype._add = function (params) {
             var container = this;
-            var selector = params.selector;
+            var className = params.className;
             var elementTag = params.tag;
-            var data = params.data || [selector];
-
+            var data = params.data || [className];
+            var exitTransition = params.exitTransition || null;
+            var enterTransition = params.enterTransition || null;
             // Pattern in action
-            var selection = container.selectAll('.' + selector).data(data, (d, i) => {
-                if (typeof d === 'object' && d.id) return d.id;
+            var selection = container.selectAll("." + className).data(data, (d, i) => {
+                if (typeof d === "object") {
+                    if (d.id) {
+                        return d.id;
+                    }
+                }
                 return i;
             });
-            selection.exit().remove();
-            selection = selection.enter().append(elementTag).merge(selection);
-            selection.attr('class', selector);
+            if (exitTransition) {
+                exitTransition(selection);
+            } else {
+                selection.exit().remove();
+            }
+
+            const enterSelection = selection.enter().append(elementTag);
+            if (enterTransition) {
+                enterTransition(enterSelection);
+            }
+            selection = enterSelection.merge(selection);
+            selection.attr("class", className);
             return selection;
         };
     }
 
-    // Render Chart
-    render() {
-        // Define containers and set SVG width based on container size
-        this.setDynamicContainer();
+    setDynamicContainer() {
+        const attrs = this.getState();
 
-        // Calculate some properties
-        this.calculateProperties();
+        //Drawing containers
+        var d3Container = d3.select(attrs.container);
+        var containerRect = d3Container.node().getBoundingClientRect();
+        if (containerRect.width > 0) attrs.svgWidth = containerRect.width;
 
-        // Draw SVG and its wrappers
-        this.drawSvgAndWrappers();
-
-        //TODO - REMOVE THIS SNIPPET AFTER YOU START THE DEVELOPMENT
-        this.drawContent();
-
-        // listen for resize event and reRender accordingly
-        this.reRenderOnResize();
-
-        // Allow chaining
-        return this;
-    }
-
-    // TODO - REMOVE THIS SNIPPET AFTER YOU START THE DEVELOPMENT
-    drawContent() {
-        const { chart, data } = this.getState();
-        chart
-            .patternify({ tag: 'text', selector: 'example-text', data: [data.message] })
-            .text(d => d)
-            .attr('x', 10)
-            .attr('y', 20);
-    }
-
-    // Listen resize event and resize on change
-    reRenderOnResize() {
-        const { id, d3Container, svgWidth } = this.getState();
-        d3.select(window).on('resize.' + id, () => {
-            const containerRect = d3Container.node().getBoundingClientRect();
-            const newSvgWidth = containerRect.width > 0 ? containerRect.width : svgWidth;
-            this.setState({ svgWidth: newSvgWidth });
+        d3.select(window).on("resize." + attrs.id, function () {
+            var containerRect = d3Container.node().getBoundingClientRect();
+            if (containerRect.width > 0) attrs.svgWidth = containerRect.width;
             this.render();
         });
+
+        this.setState({ d3Container });
     }
 
-    // Draw SVG and g wrapper
-    drawSvgAndWrappers() {
-        const { d3Container, svgWidth, svgHeight, defaultFont, calc } = this.getState();
-        const { chartLeftMargin, chartTopMargin, chartWidth, chartHeight } = calc;
+    addChartGui() {
+        const { guiEnabled, firstRender } = this.getState()
+        console.log({ guiEnabled, firstRender })
+        if (!guiEnabled || !firstRender) return;
+        if (typeof lil == 'undefined') return;
+        const gui = new lil.GUI()
+        gui.close()
+        const state = JSON.parse(JSON.stringify(this.getState()))
+        const propChanged = () => {
+            supportedKeys.forEach(k => {
+                this.setState({ [k]: state[k] })
+            })
+            this.render();
+        }
+        const supportedKeys = Object.keys(state)
+            .filter(k =>
+                typeof state[k] == 'number' ||
+                typeof state[k] == 'string' ||
+                typeof state[k] == 'boolean' 
 
-        // Draw SVG
-        const svg = d3Container
-            .patternify({ tag: 'svg', selector: 'svg-chart-container' })
-            .attr('width', svgWidth)
-            .attr('height', svgHeight)
-            .attr('font-family', defaultFont);
+            )
+            .filter(d => !['guiEnabled', 'firstRender'].includes(d))
+        console.log({ supportedKeys, state })
+        supportedKeys.forEach(key => {
+            gui.add(state, key).onChange(d => {
+                propChanged()
+            })
+        })
 
-        //Add container g element
-        var chart = svg
-            .patternify({ tag: 'g', selector: 'chart' })
-            .attr('transform', 'translate(' + chartLeftMargin + ',' + chartTopMargin + ')');
-
-        this.setState({ chart, svg })
     }
-
-    // Calculate some properties
-    calculateProperties() {
-        const { marginTop, marginLeft, marginRight, marginBottom, svgWidth, svgHeight } = this.getState();
-
-        // Calculated properties
-        var calc = {
-            id: this.createId(), // id for event handlings,
-            chartTopMargin: marginTop,
-            chartLeftMargin: marginLeft,
-            chartWidth: svgWidth - marginRight - marginLeft,
-            chartHeight: svgHeight - marginBottom - marginTop
-        };
-
-        this.setState({ calc })
-    }
-
-    // Set dynamic width for chart
-    setDynamicContainer() {
-        const { container, svgWidth } = this.getState();
-
-        // Drawing containers
-        const d3Container = d3.select(container);
-        var containerRect = d3Container.node().getBoundingClientRect();
-        let newSvgWidth = containerRect.width > 0 ? containerRect.width : svgWidth;
-        this.setState({ d3Container, svgWidth: newSvgWidth });
-    }
-
-    // Expose method for smoothly updating data
-    updateData(newData) {
-        const { data } = this.getChartState();
-        return this;
-    }
-
 }
